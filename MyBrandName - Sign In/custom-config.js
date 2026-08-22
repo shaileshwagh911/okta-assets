17e6c7cf0aa50cff0b9e60d8cb4c0b41a1cd70ea
shaileshwagh911 <42466695+shaileshwagh911@users.noreply.github.com>
2026-08-21 21:47:30 -0400

    Update custom-config.js

--- MyBrandName - Sign In/custom-config.js
+++ MyBrandName - Sign In/custom-config.js
@@ -18,6 +18,14 @@ function getCurrentConfigs () {
       CREATE_ACCOUNT_LINK: "https://onlineservice.cinf.net/b2c/SelfService/create-account",
       ENV: "qat"
     },
+    "twdb.demoshailesh.com": {
+      WEB_CLIENT_ID: "0oa2ssofb6lrnDNyK0h8",
+      MOBILE_CLIENT_ID: "0oa2sso5w9bYE6l1n0h8",
+      HELP_PAGE_LINK: "https://qatonlineservice.cinfin.com/b2c/Account_Self_Service/ContactCustomerSupport.aspx",
+      HELP_PAGE_1_LINK: "https://www.cinf.net/phhelp",
+      CREATE_ACCOUNT_LINK: "https://onlineservice.cinf.net/b2c/SelfService/create-account",
+      ENV: "qat"
+    }
   };
 
   // Get current Okta tenant hostname
